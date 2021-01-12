47ff96ab5451484a02091bc165b5f6e427ae2ed8
Hiroyuki Tanaka <pillyshi21@gmail.com>
2021-01-12 23:24:42 +0900

    Add type hints of ImageClassifier

--- src/image_classifier/model.pyi
+++ src/image_classifier/model.pyi
@@ -1,10 +1,15 @@
-from typing import List
+from typing import Tuple, List
 
 import numpy as np
 
 
 class ImageClassifier:
 
-    def train(self, images: List[np.ndarray], labels: List[str]): ...
+    def __init__(self, window: Tuple[int, int], stride: Tuple[int, int], n_estimators: int, random_state: int):
+        ...
 
-    def classify(self, images: List[np.ndarray]) -> List[str]: ...
+    def train(self, images: List[np.ndarray], labels: List[str]):
+        ...
+
+    def classify(self, images: List[np.ndarray]) -> List[str]:
+        ...
